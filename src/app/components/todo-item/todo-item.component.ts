25a1a6a08d312894e13e800bfd8add208a8e2d1c
robyn <65057146+robynsdev@users.noreply.github.com>
2021-02-15 21:58:09 +1100

    todo-item base onToggle & onDelete

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -12,4 +12,21 @@ export class TodoItemComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  // Set Dynamic classes
+  setClasses() {
+    let classes = {
+      todo: true,
+      'is-complete': this.todo.completed,
+    };
+    return classes;
+  }
+
+  onToggle(todo) {
+    todo.completed = !todo.completed;
+  }
+
+  onDelete(todo) {
+    console.log('deleted');
+  }
 }
