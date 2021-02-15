b313fcdca54aebb9b83579a7a5a34a8d2385e690
robyn <65057146+robynsdev@users.noreply.github.com>
2021-02-15 21:29:58 +1100

    created todo.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-crash-todo';
+  greet: string = 'hello';
+
+  constructor() {
+    console.log(123);
+  }
+
+  changeName(name: string): void {
+    this.name = name;
+  }
 }
